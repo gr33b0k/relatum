f26c9ea01ae7fa6db179862e67019e6f291bf980
Ilya <sovetniy@bk.ru>
2026-01-27 15:14:49 +0300

    feat: integrate InteractionState to CanvasDrawer

--- src/index.js
+++ src/index.js
@@ -12,12 +12,14 @@ const nodeB = new Node({ id: "2", label: "Node B", x: 200, y: 150 });
 const nodeC = new Node({ id: "3", label: "Node C", x: 400, y: 300 });
 const nodeD = new Node({ id: "4", label: "Node D", x: 600, y: 250 });
 const nodeE = new Node({ id: "5", label: "Node E", x: 800, y: 100 });
+const nodeF = new Node({ id: "6", label: "Node F", x: 1000, y: 500 });
 
 graph.addNode(nodeA);
 graph.addNode(nodeB);
 graph.addNode(nodeC);
 graph.addNode(nodeD);
 graph.addNode(nodeE);
+graph.addNode(nodeF);
 
 graph.linkNodes(nodeA, nodeB);
 graph.linkNodes(nodeA, nodeC);
@@ -27,7 +29,7 @@ graph.linkNodes(nodeA, nodeE);
 const canvas = document.getElementById("graphCanvas");
 const camera = new Camera();
 const interactionState = new InteractionState();
-const drawer = new CanvasDrawer(canvas, camera);
+const drawer = new CanvasDrawer(canvas, camera, interactionState);
 const interaction = new Interaction(canvas, graph, camera, interactionState);
 
 function renderGraph() {
