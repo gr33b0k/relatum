f29509a3382d07979978010e56bbb19976f3b3cf
Ilya <sovetniy@bk.ru>
2026-01-27 13:44:32 +0300

    feat: add coordinates and weight to Node

--- src/models/Node.js
+++ src/models/Node.js
@@ -1,9 +1,12 @@
 export class Node {
-  constructor({ id, label }) {
+  constructor({ id, label, x = 0, y = 0, weight = 1 }) {
     if (!id) {
       throw new Error("Node must have a valid id.");
     }
     this.id = id;
     this.label = label;
+    this.x = x;
+    this.y = y;
+    this.weight = weight;
   }
 }
