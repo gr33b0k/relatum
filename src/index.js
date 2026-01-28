cd1324ee6baa565a1515967cc1b9f70f1502a7b1
Ilya <sovetniy@bk.ru>
2026-01-28 19:01:04 +0300

    refactor: remove unnecessary button to toggle sidebar

--- src/index.js
+++ src/index.js
@@ -44,13 +44,10 @@ const interaction = new Interaction(
 
 const sidebar = document.querySelector(".sidebar");
 const overlay = document.querySelector(".overlay");
-const toggle = document.querySelector(".sidebartoggle");
 const closeSidebarButton = document.getElementById("closeSidebar");
 const sidebarController = new SidebarController(sidebar, overlay);
 
 interaction.onNodeSelect = () => sidebarController.changeState();
-
-toggle.addEventListener("click", () => sidebarController.changeState());
 
 closeSidebarButton.addEventListener("click", () =>
   sidebarController.changeState(),
