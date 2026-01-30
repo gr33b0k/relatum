e52404feab02ddb25f83fe03d55a3efd38f69b45
Ilya <sovetniy@bk.ru>
2026-01-31 00:52:21 +0300

    feat: display description and tags of nodes in sidebar

--- src/index.js
+++ src/index.js
@@ -10,12 +10,74 @@ import { SidebarController } from "./ui/SidebarController.js";
 
 const graph = new Graph();
 
-const nodeA = new Node({ id: "1", label: "Node A", x: 50, y: 50 });
-const nodeB = new Node({ id: "2", label: "Node B", x: 200, y: 150 });
-const nodeC = new Node({ id: "3", label: "Node C", x: 400, y: 300 });
-const nodeD = new Node({ id: "4", label: "Node D", x: 600, y: 250 });
-const nodeE = new Node({ id: "5", label: "Node E", x: 800, y: 100 });
-const nodeF = new Node({ id: "6", label: "Node F", x: 1000, y: 500 });
+const nodeA = new Node({
+  id: "1",
+  label: "Node A",
+  x: 50,
+  y: 50,
+  description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis aperiam odit aliquid nesciunt voluptates consequuntur aspernatur, quod nulla cumque. Enim fugit numquam mollitia sequi reiciendis dolorum veritatis vitae amet perferendis?
+  Totam repudiandae modi, maiores maxime optio voluptatem iusto unde assumenda quis, commodi voluptas quasi. In, voluptatibus cumque vero debitis commodi eius voluptas sunt facilis saepe, possimus dolore sint, obcaecati illo!
+  Dolorum, dicta. Sapiente distinctio rerum molestias quas ratione blanditiis, aut sint aliquam reiciendis eligendi quisquam cupiditate soluta enim commodi, architecto aperiam numquam dolor vero error quasi minima? Magni, itaque sunt.
+  Officia, impedit. Neque eveniet corrupti voluptas nesciunt expedita officia incidunt, laborum rem sunt ducimus enim ut est a nam quia. Blanditiis labore vero eaque dolores quam commodi, quasi exercitationem sapiente?
+  Odio fugiat eveniet eos consectetur eum excepturi, amet aut, ducimus a, doloremque velit? Aliquam nemo vitae sed magni ducimus nesciunt nulla cum explicabo! Atque perferendis ullam reprehenderit, impedit eius temporibus!
+  Unde voluptatibus dolore ex eaque non perferendis, labore debitis. Nesciunt, quis impedit cumque nemo autem ipsum a sunt sed dolorem, soluta totam. Laudantium maiores aperiam harum labore quisquam esse recusandae!`,
+  tags: ["frontend", "basics"],
+});
+const nodeB = new Node({
+  id: "2",
+  label: "Node B",
+  x: 200,
+  y: 150,
+  description: `Lorem ipsum dolor sit amet consectetur, 
+                adipisicing elit. Delectus quam blanditiis, 
+                doloremque officia odio aut saepe aspernatur doloribus, 
+                explicabo, dolores magni enim quos maxime maiores qui. 
+                Excepturi quisquam fugiat veritatis.`,
+  tags: ["javascript"],
+});
+const nodeC = new Node({
+  id: "3",
+  label: "Node C",
+  x: 400,
+  y: 300,
+  description: `Lorem ipsum dolor sit amet consectetur`,
+  tags: ["css"],
+});
+const nodeD = new Node({
+  id: "4",
+  label: "Node D",
+  x: 600,
+  y: 250,
+  description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Vitae ad illum pariatur. Illum, mollitia ducimus! 
+                Explicabo provident id eius nobis. 
+                Impedit id officia corporis magnam delectus repellat in sit. Expedita?`,
+  tags: ["html"],
+});
+const nodeE = new Node({
+  id: "5",
+  label: "Node E",
+  x: 800,
+  y: 100,
+  description: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                Beatae obcaecati aspernatur dolorum, vitae ab molestiae accusantium voluptas, omnis architecto id minima. 
+                Maiores quae delectus deserunt impedit sed sunt non sit?
+                Quos, ipsa magni cupiditate ab adipisci nulla nemo dolore quas laudantium molestias, 
+                veritatis sit ipsum aspernatur nostrum error sed harum libero quidem veniam, officia corrupti? 
+                Doloribus voluptas omnis quo mollitia!`,
+  tags: ["python"],
+});
+const nodeF = new Node({
+  id: "6",
+  label: "Node F",
+  x: 1000,
+  y: 500,
+  description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Eius, debitis asperiores. Officiis, sequi amet? 
+                Laudantium in ducimus dolores voluptates vero aspernatur fugit repellat iure. 
+                Illo, dolorum error. Magni, placeat! Officia!`,
+  tags: ["backend", "basics"],
+});
 
 graph.addNode(nodeA);
 graph.addNode(nodeB);
@@ -27,7 +89,7 @@ graph.addNode(nodeF);
 graph.linkNodes(nodeA, nodeB);
 graph.linkNodes(nodeA, nodeC);
 graph.linkNodes(nodeA, nodeD);
-graph.linkNodes(nodeA, nodeE);
+graph.linkNodes(nodeE, nodeF);
 
 const canvas = document.getElementById("graphCanvas");
 const camera = new Camera();
@@ -47,7 +109,9 @@ const overlay = document.querySelector(".overlay");
 const closeSidebarButton = document.getElementById("closeSidebar");
 const sidebarController = new SidebarController(sidebar, overlay);
 
-interaction.onNodeSelect = () => sidebarController.changeState();
+interaction.onNodeSelect = (selectedNode) => {
+  sidebarController.changeState(selectedNode);
+};
 
 closeSidebarButton.addEventListener("click", () =>
   sidebarController.changeState(),
--- src/ui/SidebarController.js
+++ src/ui/SidebarController.js
@@ -1,7 +1,18 @@
 export class SidebarController {
+  sidebar = null;
+  overlay = null;
+
+  #title = null;
+  #description = null;
+  #tags = null;
+
   constructor(sidebar, overlay) {
     this.sidebar = sidebar;
     this.overlay = overlay;
+
+    this.#title = sidebar.querySelector(".sidebar__title");
+    this.#description = sidebar.querySelector(".sidebar__description-text");
+    this.#tags = sidebar.querySelector(".sidebar__tag-list");
 
     this.#init();
   }
@@ -10,8 +21,29 @@ export class SidebarController {
     this.overlay.addEventListener("click", () => this.changeState());
   }
 
-  changeState() {
+  changeState(selectedNode) {
     this.sidebar.classList.toggle("sidebar--hidden");
     this.overlay.classList.toggle("hidden");
+    if (!selectedNode) {
+      return;
+    }
+    this.#renderNodeInfo(selectedNode);
   }
+
+  #renderNodeInfo(node) {
+    this.#title.textContent = node.label;
+    this.#description.textContent = node.description
+      ? node.description
+      : "This node have no description";
+
+    this.#tags.innerHTML = "";
+    node.tags.forEach(this.#renderNodeTag);
+  }
+
+  #renderNodeTag = (tag) => {
+    const li = document.createElement("li");
+    li.className = "tag";
+    li.textContent = tag;
+    this.#tags.appendChild(li);
+  };
 }
