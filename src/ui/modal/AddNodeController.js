7924cecf63cb9684b039c9da1677bafb5e30800b
Ilya <sovetniy@bk.ru>
2026-02-21 12:35:52 +0300

    feat: add title to direction buttons

--- src/ui/modal/AddNodeController.js
+++ src/ui/modal/AddNodeController.js
@@ -167,6 +167,9 @@ export class AddNodeController {
     const box = document.createElement("span");
     box.className = `direction__box direction__box--${type}`;
 
+    const typeStr = String(type);
+    box.title = `${typeStr[0].toUpperCase()}${typeStr.slice(1)} this note`;
+
     box.innerHTML = `
       <svg viewBox="0 0 256 256">
         <path d="M224,128a8,8,0,0,1-8,8H59.31l58.35,58.34a8,8,0,0,1-11.32,11.32l-72-72a8,8,0,0,1,0-11.32l72-72a8,8,0,0,1,11.32,11.32L59.31,120H216A8,8,0,0,1,224,128Z"></path>
