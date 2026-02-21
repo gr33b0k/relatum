a3d4a1c2f5489a63cadbe9c184c3bae5d83390a5
Ilya <sovetniy@bk.ru>
2026-02-21 12:47:33 +0300

    feat: generate random coordinates when not provided

--- src/models/Node.js
+++ src/models/Node.js
@@ -2,8 +2,8 @@ export class Node {
   constructor({ label, x = 0, y = 0, weight = 1, description, tags = [] }) {
     this.id = crypto.randomUUID();
     this.label = label;
-    this.x = x;
-    this.y = y;
+    this.x = x ? x : Math.random() * 500;
+    this.y = y ? y : Math.random() * 500;
     this.vx = 0;
     this.vy = 0;
     this.weight = weight;
