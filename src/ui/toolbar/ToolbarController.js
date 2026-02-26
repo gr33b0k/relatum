a97ee5a5296d0a50d736d75e010949ef55a51193
Ilya <sovetniy@bk.ru>
2026-02-26 20:15:58 +0300

    feat: change search button icon when its clicked

--- src/ui/toolbar/ToolbarController.js
+++ src/ui/toolbar/ToolbarController.js
@@ -1,3 +1,5 @@
+import { ToolbarSearch } from "./ToolbarSearch.js";
+
 export class ToolbarController {
   onAddNode = null;
   onSearch = null;
@@ -5,6 +7,11 @@ export class ToolbarController {
 
   constructor(toolbar) {
     this.toolbar = toolbar;
+    this.searchSection = toolbar.querySelector(".toolbar__search");
+    this.toolbarSearch = new ToolbarSearch(this.searchSection);
+    this.toolbarSearch.onSearch = (query) => {
+      this.onSearch?.(query);
+    };
     this.buttons = toolbar.querySelectorAll(".toolbar__button");
     this.linkButton = this.toolbar.querySelector(
       ".toolbar__button--link-nodes",
@@ -26,7 +33,7 @@ export class ToolbarController {
     );
 
     switch (mode) {
-      case "link":
+      case "connect":
         this.linkButton.classList.add("toolbar__button--active");
         break;
       case "cursor":
@@ -38,23 +45,34 @@ export class ToolbarController {
       case "search":
         this.searchButton.classList.add("toolbar__button--active");
         break;
+      case "clear":
+        this.buttons.forEach((button) =>
+          button.classList.remove("toolbar__button--active"),
+        );
+        break;
     }
   }
 
   #initEvents() {
     this.linkButton.addEventListener("click", () => {
-      this.#setActiveMode("link");
+      this.#setActiveMode("connect");
       this.onModeChange?.("connect");
+      this.toolbarSearch.close();
+      this.#setSearch();
     });
 
     this.cursorButton.addEventListener("click", () => {
       this.#setActiveMode("cursor");
       this.onModeChange?.("cursor");
+      this.toolbarSearch.close();
+      this.#setSearch();
     });
 
     this.deleteNodeButton.addEventListener("click", () => {
       this.#setActiveMode("delete");
       this.onModeChange?.("delete");
+      this.toolbarSearch.close();
+      this.#setSearch();
     });
 
     this.addNodeButton.addEventListener("click", () => {
@@ -63,9 +81,38 @@ export class ToolbarController {
     });
 
     this.searchButton.addEventListener("click", () => {
-      this.#setActiveMode("search");
+      this.toolbarSearch.toggle();
+      this.#setSearch();
       this.onModeChange?.("cursor");
       this.onSearch?.();
     });
+  }
+
+  #setSearch() {
+    if (this.toolbarSearch.isOpen()) {
+      this.#setActiveMode("search");
+      this.searchButton.innerHTML = `
+          <svg 
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 256 256"
+          >
+            <path 
+              d="M205.66,194.34a8,8,0,0,1-11.32,11.32L128,139.31,61.66,205.66a8,8,0,0,1-11.32-11.32L116.69,128,50.34,61.66A8,8,0,0,1,61.66,50.34L128,116.69l66.34-66.35a8,8,0,0,1,11.32,11.32L139.31,128Z"
+            ></path>
+          </svg>`;
+    } else {
+      this.#setActiveMode("clear");
+      this.searchButton.innerHTML = `
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 256 256"
+          >
+            <path
+              d="M229.66,218.34l-50.07-50.06a88.11,88.11,0,1,0-11.31,11.31l50.06,50.07a8,8,0,0,0,11.32-11.32ZM40,112a72,72,0,1,1,72,72A72.08,72.08,0,0,1,40,112Z"
+            ></path>
+          </svg>`;
+    }
   }
 }
